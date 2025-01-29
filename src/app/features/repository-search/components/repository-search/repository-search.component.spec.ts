a4e6c5a9cbe60f39d96a9e98f18e2b71857f4632
iamdave86 <5939607+iamdave86@users.noreply.github.com>
2025-01-29 21:19:55 +0800

    feat(repository-search)
    Trigger API call when clicking on the search button, set the input readonly, the button disabled, and show spinner when loading.

--- src/app/features/repository-search/components/repository-search/repository-search.component.spec.ts
+++ src/app/features/repository-search/components/repository-search/repository-search.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { RepositorySearchComponent } from './repository-search.component';
 import { GithubApiService } from '@shared/services/github-api/github-api.service';
@@ -11,7 +13,7 @@ describe('RepositorySearchComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule],
-      providers: [GithubApiService],
+      providers: [GithubApiService, provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RepositorySearchComponent);
--- src/app/features/repository-search/components/repository-search/repository-search.component.ts
+++ src/app/features/repository-search/components/repository-search/repository-search.component.ts
@@ -1,29 +1,43 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
+import { GithubApiService } from '@shared/services/github-api/github-api.service';
 
 @Component({
   selector: 'app-repository-search',
   templateUrl: './repository-search.component.html',
   styleUrls: ['./repository-search.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, SubHeaderComponent, ReactiveFormsModule, MatIconModule, MatInputModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    SubHeaderComponent,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class RepositorySearchComponent {
   public form: FormGroup;
+  public isLoading$: Observable<boolean>;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private githubApiService: GithubApiService, private formBuilder: FormBuilder) {
+    this.isLoading$ = this.githubApiService.isLoading;
     this.form = this.initForm();
   }
 
   public search() {
     if (this.form.valid) {
       const { search } = this.form.value;
+      this.githubApiService.getRepositoriesByName(search).subscribe(console.log);
     }
   }
 
