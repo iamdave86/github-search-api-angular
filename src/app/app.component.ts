bc3ffbce15418ddaa559370d223e0f257e1d0acd
iamdave86 <5939607+iamdave86@users.noreply.github.com>
2025-01-29 23:35:54 +0800

    feat(app)
    Make main header a link.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
 })
 export class AppComponent {}
